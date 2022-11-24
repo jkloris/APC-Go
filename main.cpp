5d23dcecf51af30cae7b578a624acfd7d79aa3b2
jkloris <kapusta.jergus@gmail.com>
2022-11-24 14:01:45 +0100

    priebeh hry

--- main.cpp
+++ main.cpp
@@ -1,5 +1,6 @@
 
 #include <iostream>
+#include <string>
 #include <vector>
 
 #define EMPTY 0
@@ -14,41 +15,39 @@ std::vector<std::vector<uint64_t>> initBoard(uint64_t size) {
 	return board;
 }
 
+struct Args
+{
+	bool board;
+	bool score;
+	unsigned int size;
+};
+
 void printBoard(std::vector<std::vector<uint64_t>> board) {
 	for (size_t i = 0; i < board.size(); i++)
 	{
-		/*if (i == 0) {
-			std::cout << " ";
-		}
-		else {
-			std::cout << i;
-		}*/
 
 		for (size_t j = 0; j < board.size(); j++) {
-			/*if (i == 0) {
-				std::cout << " " << j;
-				continue;
-			}*/
+
 				
 			switch (board[i][j]%10)
 			{
 			case EMPTY:
-				std::cout << " .";
+				std::cout << ".";
 				break;
 			case EMPTY_VIS:
-				std::cout << " ,";
+				std::cout << ",";
 				break;
 			case STONE_O:
-				std::cout << " O";
+				std::cout << "O";
 				break;
 			case STONE_O_VIS:
-				std::cout << " o";
+				std::cout << "o";
 				break;
 			case STONE_X:
-				std::cout << " X";
+				std::cout << "X";
 				break;
 			case STONE_X_VIS:
-				std::cout << " x";
+				std::cout << "x";
 				break;
 			default:
 				break;
@@ -109,11 +108,11 @@ void clearVisits(std::vector<std::vector<uint64_t>>* board) {
 
 
 
-void place(std::vector<std::vector<uint64_t>>* board, uint64_t x, uint64_t y, uint16_t stone) {
+bool place(std::vector<std::vector<uint64_t>>* board, uint64_t x, uint64_t y, uint16_t stone) {
 	
 	if ((*board)[x][y] != EMPTY) {
-		std::cout << "Spot is not empty\n";
-		return;
+		//std::cout << "Spot is not empty\n";
+		return false;
 	}
 
 	uint16_t invStone = stone == STONE_O ? STONE_X : STONE_O;
@@ -129,92 +128,140 @@ void place(std::vector<std::vector<uint64_t>>* board, uint64_t x, uint64_t y, ui
 
 
 	if (x > 0 && boardCpy[x - 1][y] == invStone && findFreedom(&boardCpy, x - 1, y, invStone) == 0) {
-		std::cout << "DELx-1\n";
 		clearStones(&boardCpy, x - 1,y, invStone+1);
 		cleared = true;
 	}
 	clearVisits(&boardCpy);
 
 	if (y > 0 && boardCpy[x][y - 1] == invStone && findFreedom(&boardCpy, x, y - 1, invStone) == 0) {
-		std::cout << "DELy-1\n";
 		clearStones(&boardCpy, x, y-1, invStone+1);
 		cleared = true;
 	}
 	clearVisits(&boardCpy);
 
 	if (x < boardCpy.size() - 1 && boardCpy[x + 1][y] == invStone && findFreedom(&boardCpy, x + 1, y, invStone) == 0) {
-		std::cout << "DELx+1\n";
 		clearStones(&boardCpy, x + 1,y, invStone+1);
 		cleared = true;
 	}
 	clearVisits(&boardCpy);
 
 	if (y < boardCpy.size() - 1 && boardCpy[x][y + 1] == invStone && findFreedom(&boardCpy, x, y + 1, invStone) == 0) {
-		std::cout << "DELy+1\n";
 		clearStones(&boardCpy, x , y+1, invStone+1);
 		cleared = true;
 	}
 	clearVisits(&boardCpy);
 
 	if (!cleared && findFreedom(&boardCpy2, x, y, stone) == 0) {
-		std::cout << "Cant place stone\n";
+		//std::cout << "Cant place stone\n";
 		//boardCpy[x][y] = EMPTY;
-		return;
+		return false;
 	}
 	boardCpy[x][y] = stone;
 	*board = boardCpy;
-
+	
+	return true;
 }
 
 
-//int main(int argc, char* argv[])
-int main()
+
+
+int main(int argc, char* argv[])
+//int main()
 {
-	std::vector<std::vector<uint64_t>> test(6, std::vector<uint64_t>(6));
-	test[0] = { 2, 2, 2, 2, 2, 2 };
-	test[1] = { 2, 4, 4, 4, 4, 2 };
-	test[2] = { 2, 4, 2, 2, 4, 2 };
-	test[3] = { 2, 4, 0, 2, 4, 2 };
-	test[4] = { 2, 4, 4, 4, 4, 2 };
-	test[5] = { 2, 2, 0, 2, 2, 2 };
-	//printBoard(test);
+	if (argc < 2 || argc > 4)
+		return EXIT_FAILURE;
 
-	std::vector<std::vector<uint64_t>> board = initBoard(7), boardCpy;
+	Args arguments = {};
 
-	boardCpy = test;
-	printBoard(boardCpy);
-	place(&boardCpy, 5, 2, STONE_X);
-	printBoard(boardCpy);
+	uint32_t a;
+	try {
+		for (char* c = argv[1]; *c; c++)
+			if (!std::isdigit(*c))
+				throw(std::runtime_error(argv[3]));
 
-	place(&boardCpy, 5, 3, STONE_O);
-	printBoard(boardCpy);
+		a = std::stoul(argv[1]);
+		if (a <= 0)
+			throw(std::runtime_error(argv[3]));
+	}
+	catch (std::exception) {
+		return EXIT_FAILURE;
+	}
+	arguments.size = a;
 
-	place(&boardCpy, 5, 4, STONE_O);
-	printBoard(boardCpy);
 
-	place(&boardCpy, 5, 5, STONE_X);
-	printBoard(boardCpy);
-	/*
-	board[0][1] = STONE_O;
-	board[2][4] = STONE_X;
-	board[1][6] = STONE_O;
-	board[2][5] = STONE_O;
-	board[3][5] = STONE_X;
-	board[1][5] = STONE_X;
-	board[4][5] = STONE_X;
-	board[5][6] = STONE_X;
-	board[4][6] = STONE_O;
-	board[3][6] = STONE_O;*/
+	for (int i = 2; i < argc; i++) {
+		std::string brd = "--board";
+		std::string scr = "--score";
+		if (brd.compare(argv[i])==0 && !arguments.board) {
+			arguments.board = true;
+		}
+		else if (scr.compare(argv[i]) == 0 && !arguments.score) {
+			arguments.score = true;
+		}
+		else {
+			return EXIT_FAILURE;
+		}
+	}
 
-	//printBoard(board);
-	//boardCpy = board;
-	//std::cout<< findFreedom(&boardCpy, 0, 0 , STONE_X) << '\n';
-	//printBoard(boardCpy);
-	//boardCpy = board;
-	//std::cout << findFreedom(&boardCpy, 2, 4, STONE_X) << '\n';
-	//printBoard(boardCpy);
-	//std::cout << findFreedom(&boardCpy, 2, 5, STONE_O) <<'\n';
+
 	
+	std::string cmd;
+	uint16_t end = 0;
+	bool turn = true;
+	unsigned int num = 0;
+	std::vector<unsigned int> pos = { 0,0 };
+
+	std::vector<std::vector<uint64_t>> board = initBoard(arguments.size);
+
+	while (end < 2) {
+
+		std::cin >> cmd;
+
+		if (cmd=="pass" && num == 0) {
+			end += 1;
+			num = 0;
+			turn = !turn;
+		}
+		else {
+			end = 0;
+			try
+			{
+				pos[num] = std::stoi(cmd);
+			}
+			catch (const std::exception&)
+			{
+				return EXIT_FAILURE;
+			}
+			if (pos[num] < 0 || pos[num] >= arguments.size) {
+				return EXIT_FAILURE;
+			}
+
+			if (num == 0) {
+				num++;
+
+			}else {
+				num = 0;
+				if (turn) {
+
+					if (!place(&board, pos[0], pos[1], STONE_X)) {
+						turn = !turn;
+					}
+				}
+				else {
+					if (!place(&board, pos[0], pos[1], STONE_O))
+						turn = !turn;
+				}
+				
+				turn = !turn;
+
+			}
+
+
+		}
+	}
+	if (arguments.board) {
+		printBoard(board);
+	}
 
 
 
